7d77de81c8771c3018051a26c3d4691a1dd0271c
wlawalz-del <wlawalz@gmail.com>
2025-08-19 22:11:29 +0900

    Update _utils.js

--- netlify/functions/_utils.js
+++ netlify/functions/_utils.js
@@ -6,12 +6,3 @@ export async function fetchJson(url) {
   if (!res.ok) throw new Error(`Failed to fetch ${url}: ${res.statusText}`);
   return res.json();
 }
-async function inBatches(items, size, worker, pauseMs = 0) {
-  for (let i = 0; i < items.length; i += size) {
-    const slice = items.slice(i, i + size);
-    await Promise.all(slice.map(worker));
-    if (pauseMs) await sleep(pauseMs);
-  }
-}
-
-module.exports = { CORS, ok, bad, handleOptions, fetchJson, sleep, inBatches };
