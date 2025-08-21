02425f4c3e7b0e959558bd8a2aa7f2fa6f0f145f
wlawalz-del <wlawalz@gmail.com>
2025-08-21 15:16:11 +0900

    Delete netlify/functions/_utils.js

--- netlify/functions/_utils.js
+++
@@ -1,8 +0,0 @@
-// netlify/functions/_utils.js
-import fetch from "node-fetch";
-
-export async function fetchJson(url) {
-  const res = await fetch(url);
-  if (!res.ok) throw new Error(`Failed to fetch ${url}: ${res.statusText}`);
-  return res.json();
-}
